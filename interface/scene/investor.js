1fb2ceb96720621a8e101dc37c707772c2ab4325
Javier Alvarez <javier.alvarez@taptapnetworks.com>
2013-12-10 14:54:16 +0100

    colors

--- interface/scene/investor.js
+++ interface/scene/investor.js
@@ -5,7 +5,7 @@ window.tr.scenes.investor = {};
 
 Crafty.scene('Investor', (function() {
   var self = this;
-  Crafty.background('#000000')
+  Crafty.background('#333333')
   this.bg = Crafty.e("2D, DOM")
              .attr({x:0, y: 0, w: 1200, h: 800})
 
